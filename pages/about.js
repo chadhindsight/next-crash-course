92e3315a3b496416e7170aa7a83e8d18e2a4a81b
chad hinds <chadrickj8@gmail.com>
2021-11-05 10:15:21 -0400

    Initial setup of layout component

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+            </Head>
+        </div>
+    )
+}
