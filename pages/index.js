3573addc6a0c8785c7b6cf8d5bf65fe9230c0970
chad hinds <chadrickj8@gmail.com>
2021-11-05 10:10:35 -0400

    Add head

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>WebDev Newz</title>
+      </Head>
       <h1>Welcome to Next</h1>
     </div>
   )
