8a671f7a36fd6c0a5fd0ded14e6193f026afd453
chad hinds <chadrickj8@gmail.com>
2021-11-07 09:34:22 -0500

    navigation links

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,10 @@
-const Layout = () => {
+import styles from '../styles/Layout.module.css';
 
+const Layout = ({ children }) => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>{children}</main>
+        </div>
+    )
 }
+export default Layout
