23b3f6afd4a07d36f2459bb41e95e74bd1f0f811
chad hinds <chadrickj8@gmail.com>
2021-11-22 10:59:37 -0500

    Add article related info

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,13 @@ export default function Home(articles) {
     </div>
   )
 }
-export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-  const articles = await res.json()
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
 
-  return {
-    props: {
-      articles,
-    },
-  }
-}
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
