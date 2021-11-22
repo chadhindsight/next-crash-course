525146dccd5853cd50d3dbc0543467378a24e022
chad hinds <chadrickj8@gmail.com>
2021-11-22 10:33:15 -0500

    Use header styles in header component

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
+import Nav from './Nav';
 import styles from '../styles/Layout.module.css';
 
 const Layout = ({ children }) => {
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>{children}</main>
-        </div>
+        <>
+            <div className={styles.container}>
+                <main className={styles.main}>{children}</main>
+            </div>
+        </>
     )
 }
 export default Layout
