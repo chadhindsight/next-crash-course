d194a44d99dc316475af5c49463b21b0ca3a338a
chad hinds <chadrickj8@gmail.com>
2021-11-27 10:09:00 -0500

    Reorganize styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ArticleList from '../components/ArticleList'
 import styles from '../styles/Home.module.css'
 
 export default function Home(articles) {
@@ -10,9 +11,12 @@ export default function Home(articles) {
         <title>WebDev Newz</title>
       </Head>
       <h1>Welcome to Next</h1>
+
+      <ArticleList articels={articles} />
     </div>
   )
 }
+
 // export const getStaticProps = async () => {
 //   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
 //   const articles = await res.json()
