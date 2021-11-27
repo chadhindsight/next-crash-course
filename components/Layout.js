61afc5d85c4dc2f0a61dc5b9d492eaf3455aa767
chad hinds <chadrickj8@gmail.com>
2021-11-27 10:36:57 -0500

    Regoncfigure Layout component to include Nav, Meta and Header components

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,21 @@
-import Nav from './Nav';
-import styles from '../styles/Layout.module.css';
+import Nav from './Nav'
+import Meta from './Meta'
+import Header from './Header'
+import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
     return (
         <>
+            <Meta />
+            <Nav />
             <div className={styles.container}>
-                <main className={styles.main}>{children}</main>
+                <main className={styles.main}>
+                    <Header />
+                    {children}
+                </main>
             </div>
         </>
     )
 }
+
 export default Layout
