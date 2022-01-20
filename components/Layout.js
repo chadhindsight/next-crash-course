0bd82116a4b13ebc3667a2e16c2887d0af898681
chad hinds <chadrickj8@gmail.com>
2022-01-19 21:18:55 -0500

    Remove comment

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Nav from './Nav'
 import Meta from './Meta'
 import Header from './Header'
 import styles from '../styles/Layout.module.css'
-// Crash
+
 const Layout = ({ children }) => {
     return (
         <>
