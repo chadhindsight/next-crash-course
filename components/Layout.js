46b1409d61550454f213e8abe5ea6ec0687d6f4a
chad hinds <chadrickj8@gmail.com>
2022-01-19 21:28:10 -0500

    Refactor imports

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,9 @@
-import Nav from './Nav'
-import Meta from './Meta'
-import Header from './Header'
-import styles from '../styles/Layout.module.css'
+import Nav from './Nav';
+import Meta from './Meta';
+import Header from './Header';
+import styles from '../styles/Layout.module.css';
 
-const Layout = ({ children }) => {
+; const Layout = ({ children }) => {
     return (
         <>
             <Meta />
