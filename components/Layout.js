5a8b87bb228a8b6899407bf5ee9f12f30c80b4e9
chad hinds <chadrickj8@gmail.com>
2022-02-04 18:37:11 -0500

    Comment on how the Layout comp is used

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Nav from './Nav';
 import Meta from './Meta';
 import Header from './Header';
 import styles from '../styles/Layout.module.css';
-
+// Serves as the main hub
 const Layout = ({ children }) => {
     return (
         <>
