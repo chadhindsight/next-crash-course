5c685ae4df2eee3b00e390548ed75fba8f25d5f1
chad hinds <chadrickj8@gmail.com>
2022-03-28 00:57:01 -0400

    Add a comment

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Nav from './Nav';
 import Meta from './Meta';
 import Header from './Header';
 import styles from '../styles/Layout.module.css';
-
+//parent
 const Layout = ({ children }) => {
     return (
         <>
